e114aceb20d587cedbc5a0f62e97e554f1cace27
Lantsov Alexandr <lantsov.alexandr.2@gmail.com>
2020-09-08 12:16:19 +0300

    Small changes in: index.html, projects.html, about.html and main.js file

--- js/main.js
+++ js/main.js
@@ -1,30 +1,33 @@
+// Smoth scroll for scroll button
 let scroll = new SmoothScroll('a[href*="#"]', {
    speed: 1600,
    easing: "easeInOutCubic",
 });
-let contactPage = "../pages/contacts.html";
-let projectPage = "../pages/projects.html";
-let aboutPage = "../pages/about.html";
-let writeMe = "mailto:lancov.alex.2@gmail.com";
+
+// Variables for links
+let contactPage = "../pages/contacts.html",
+    projectPage = "../pages/projects.html",
+    aboutPage = "../pages/about.html",
+    writeMe = "mailto:lancov.alex.2@gmail.com";
 
 let project1 = "#",
-   project2 = "#",
-   project3 = "#";
+    project2 = "#",
+    project3 = "#";
 
 let behance = "https://www.behance.net/lantsov_a",
-   dribbble = "https://dribbble.com/lantsov_a",
-   vk = "https://vk.com/lantsov_a",
-   telegram = "https://t.me/lantsov_a",
-   linkedin = "https://www.linkedin.com/in/lantsova/";
+    dribbble = "https://dribbble.com/lantsov_a",
+    vk = "https://vk.com/lantsov_a",
+    telegram = "https://t.me/lantsov_a",
+    linkedin = "https://www.linkedin.com/in/lantsova/";
 
 $(document).ready(function () {
-   //Mobile menu
+   //Mobile menu toogle
    $(".mobile-menu").click(function (event) {
       $(".mobile-menu, .header__menu").toggleClass("active");
       $("body").toggleClass("lock");
    });
 
-   //Buttons
+   //Global buttons
    $(".start_project").click(function (event) {
       $(location).attr("href", contactPage);
    });
@@ -52,7 +55,7 @@ $(document).ready(function () {
       $(location).attr("href", project3);
    });
 
-   //Social buttons
+   //Social buttons in footer
    $("#behance").click(function (event) {
       $(location).attr("href", behance);
    });
